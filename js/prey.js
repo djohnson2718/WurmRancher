ab1647c0ce38049cc4636658aa351433f62468cf
Drew Johnson <werd2.718@gmail.com>
2023-01-10 16:40:24 -0700

    Revert "more work on predator/prey"
    This reverts commit 9e6ee570ab4698b512a73d63801175bf07fd2481.

--- js/prey.js
+++ js/prey.js
@@ -28,52 +28,11 @@ export function Prey(base) {
         ;
     };
 }
-const tol = 1;
 export function Predator(base) {
     return class Predator_ extends base {
         PreyStolen() {
         }
-        Update(time_step) {
-            if (this.hit) {
-                super.Update(time_step);
-                return;
-            }
-            if (this.target != null && DistanceObjects(this, this.target) < tol) {
-                this.target.Eat();
-                if (this.taraget.Eaten)
-                    this.target = null;
-            }
-            if (this.target == null && this.resting) {
-                //request to chase closest available
-            }
-            super.Update(time_step);
-        }
     };
-}
-export function GetClosestPrey(to, preyNames) {
-    let best_dist_so_far = Number.MAX_VALUE;
-    let closest = null;
-    let f = null;
-    let cur_dist;
-    //console.log("looking for prey");
-    for (const e of GameElements) {
-        //console.log(e, e.Name,e.Name==preyName);
-        if (e.Name == preyName) {
-            f = e;
-            //console.log("available", f.Available(care_about_dibs));
-            if (f.Available(care_about_dibs)) {
-                //console.log("available!")
-                cur_dist = DistanceObjects(f, to);
-                if (cur_dist < best_dist_so_far) {
-                    //console.log("new best");
-                    closest = f;
-                    best_dist_so_far = cur_dist;
-                }
-            }
-        }
-    }
-    //console.log("found",closest);
-    return closest;
 }
 let FeederP = Prey(Feeder);
 let r = new FeederP();
--- ts/prey.ts
+++ ts/prey.ts
@@ -1,7 +1,6 @@
 import { Feeder } from "./feeder.js";
 import { DistanceObjects } from "./gameControl.js";
 import { GameElement } from "./gameElement.js";
-import { LaserHitable } from "./laserHitable.js";
 import { MovesToDestinationControl } from "./movesToDestinationControl.js";
 import { HasCenter, OnTheFieldPiece } from "./OnTheFieldPiece.js";
 
@@ -9,8 +8,8 @@ export interface PreyI extends OnTheFieldPiece{
     Available(care_about_dibs:boolean) : boolean;
 }
 
-type Preyable = new (...args: any[]) => MovesToDestinationControl & {Name :string, Layer:number, Eat():number, get Eaten() : boolean};
-type Predatorable = new (...args: any[]) => LaserHitable & {Name:string, Layer:number};
+type MovesToDestinationControlClass = new (...args: any[]) => MovesToDestinationControl & {Name :string, Layer:number};
+
 //type HasCenterClass = new (...args: any[]) => HasCenter;
 
 interface PredatorI extends MovesToDestinationControl{
@@ -22,7 +21,7 @@ class PredDist{
     dist : number;
 }
 
-export function Prey(base : Preyable){
+export function Prey(base : MovesToDestinationControlClass){
     return class Prey_ extends base{
         Available(care_about_dibs: boolean): boolean {
             return true;
@@ -48,66 +47,14 @@ export function Prey(base : Preyable){
     }
 }
 
-const tol = 1;
-
-export function Predator(base : Predatorable){
+export function Predator(base : MovesToDestinationControlClass){
     return class Predator_ extends base implements PredatorI{
         PreyStolen() :void{
 
         }
-
-        target : PreyI;
-        Update(time_step: number): void {
-            if (this.hit){
-                super.Update(time_step);
-                return;
-            }
-
-            if (this.target != null && DistanceObjects(this,this.target) < tol)
-            {
-                this.target.Eat();
-                if (this.taraget.Eaten)
-                    this.target = null;
-            }
-
-            if (this.target == null && this.resting)
-            {
-                //request to chase closest available
-            }
-            super.Update(time_step);
-        }
     }
 }
 
-export function GetClosestPrey(to: PredatorI, preyNames : Array<string>) : PreyI{
-    let best_dist_so_far = Number.MAX_VALUE;
-    let closest : Prey = null;
-    let f : Prey  = null;
-    let cur_dist : number;
-    //console.log("looking for prey");
-    for (const e of GameElements)
-    {
-        //console.log(e, e.Name,e.Name==preyName);
-        if (e.Name == preyName)
-        {
-            f = (e as unknown) as Prey;
-            //console.log("available", f.Available(care_about_dibs));
-            if (f.Available(care_about_dibs))
-            {
-                //console.log("available!")
-                cur_dist = DistanceObjects(f, to);
-                if (cur_dist < best_dist_so_far)
-                {
-                    //console.log("new best");
-                    closest = f;
-                    best_dist_so_far = cur_dist;
-                }
-            }
-        }
-    }
-    //console.log("found",closest);
-    return closest;
-}
 
 let FeederP = Prey(Feeder);
 
