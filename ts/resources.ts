63be79d6d4bc08aaf46da03536f3e0a3d851389f
Drew Johnson <werd2.718@gmail.com>
2023-01-11 19:47:09 -0700

    Update resources.ts

--- ts/resources.ts
+++ ts/resources.ts
@@ -1,79 +1,79 @@
-export const BlazeOn = new Audio("/Resources/DST-BlazeOn.mp3");
-export const InCircles = new Audio("/Resources/DST-InCircles.mp3");
-export const RockX = new Audio("/Resources/DST-RockX.mp3");
+export const BlazeOn = new Audio("Resources/DST-BlazeOn.mp3");
+export const InCircles = new Audio("Resources/DST-InCircles.mp3");
+export const RockX = new Audio("Resources/DST-RockX.mp3");
 
 
-export const laserSound = new Audio("/Resources/39459__the-bizniss__laser.wav");
+export const laserSound = new Audio("Resources/39459__the-bizniss__laser.wav");
 
-export const GrassEaterDeathSound = new Audio("/Resources/zapsplat_cartoon_wet_squelchy_pop_high_pitched_76642.mp3")
+export const GrassEaterDeathSound = new Audio("Resources/zapsplat_cartoon_wet_squelchy_pop_high_pitched_76642.mp3")
 
-export const dragonSound = new Audio("/Resources/4504__noisecollector__dragon1.wav");
-export const seedSound = new Audio("/Resources/99721__menegass__seedm2.wav");
-//export const gruntzSound = new Audio("/Resources/104697__grunz__grunz-ow.wav");
-export const monsterDieSound = new Audio("/Resources/76972__michel88__pains.wav");
-export const bigMonsterEyeHitSound = new Audio("/Resources/76965__michel88__painb.wav");
-export const monsterEatSound = new Audio("/Resources/76973__michel88__piggrunt1.wav");
-export const electic_buzz = new Audio("/Resources/55829__sergenious__electric.wav");
-export const apple_crunchSound = new Audio("/Resources/20279__koops__apple-crunch-16(1).wav");
-export const spraySound = new Audio("/Resources/13131__looppool__spraycan.wav");
-export const squishSound = new Audio("/Resources/72624__anechoix__squish3.wav");
+export const dragonSound = new Audio("Resources/4504__noisecollector__dragon1.wav");
+export const seedSound = new Audio("Resources/99721__menegass__seedm2.wav");
+//export const gruntzSound = new Audio("Resources/104697__grunz__grunz-ow.wav");
+export const monsterDieSound = new Audio("Resources/76972__michel88__pains.wav");
+export const bigMonsterEyeHitSound = new Audio("Resources/76965__michel88__painb.wav");
+export const monsterEatSound = new Audio("Resources/76973__michel88__piggrunt1.wav");
+export const electic_buzz = new Audio("Resources/55829__sergenious__electric.wav");
+export const apple_crunchSound = new Audio("Resources/20279__koops__apple-crunch-16(1).wav");
+export const spraySound = new Audio("Resources/13131__looppool__spraycan.wav");
+export const squishSound = new Audio("Resources/72624__anechoix__squish3.wav");
 
-export const parasiteAttachSound = new Audio("/Resources/125647__tec-studios__insects-noise-010.wav");
-export const parasiteDieSound = new Audio("/Resources/125648__tec-studios__insects-noise-011.wav");
-export const parasiteEatSound = new Audio("/Resources/125646__tec-studios__insects-noise-009.wav");
+export const parasiteAttachSound = new Audio("Resources/125647__tec-studios__insects-noise-010.wav");
+export const parasiteDieSound = new Audio("Resources/125648__tec-studios__insects-noise-011.wav");
+export const parasiteEatSound = new Audio("Resources/125646__tec-studios__insects-noise-009.wav");
 
 
 export const SoilBeachBackground = new Image();
-SoilBeachBackground.src = "/Resources/SoilBeach0088_1_S.jpg";
+SoilBeachBackground.src = "Resources/SoilBeach0088_1_S.jpg";
 export const SoilMudBackground = new Image();
-SoilMudBackground.src = "/Resources/SoilMud0059_2_S.jpg";
+SoilMudBackground.src = "Resources/SoilMud0059_2_S.jpg";
 export const IntroScreenBackground = new Image();
-IntroScreenBackground.src = "/Resources/Plastic0027_1_S_Intro.png";
+IntroScreenBackground.src = "Resources/Plastic0027_1_S_Intro.png";
 export const SnowBackground = new Image();
-SnowBackground.src = "/Resources/Snow0099_5_S.jpg";
+SnowBackground.src = "Resources/Snow0099_5_S.jpg";
 export const SandPebblesBrownBackground = new Image();
-SandPebblesBrownBackground.src ="/Resources/SandPebbles0060_19_S.jpg";
+SandPebblesBrownBackground.src ="Resources/SandPebbles0060_19_S.jpg";
 export const SandPebblesRedBackground = new Image();
-SandPebblesRedBackground.src = "/Resources/SandPebbles0058_9_S_red_new.jpg";
+SandPebblesRedBackground.src = "Resources/SandPebbles0058_9_S_red_new.jpg";
 export const SoilCrackedBackground = new Image();
-SoilCrackedBackground.src = "/Resources/SoilCracked0086_7_S.jpg";
+SoilCrackedBackground.src = "Resources/SoilCracked0086_7_S.jpg";
 
 export const parasiteImage = new Image();
-parasiteImage.src = "/Resources/parasite.png";
+parasiteImage.src = "Resources/parasite.png";
 export const big_monsterImage = new Image();
-big_monsterImage.src = "/Resources/big_monster.png";
+big_monsterImage.src = "Resources/big_monster.png";
 export const grassEaterImage = new Image();
-grassEaterImage.src = "/Resources/grass_eater.png";
+grassEaterImage.src = "Resources/grass_eater.png";
 export const rancherImage = new Image(); 
-rancherImage.src =  "/Resources/rancher.png";
+rancherImage.src =  "Resources/rancher.png";
 export const monsterImage = new Image();
-monsterImage.src = "/Resources/monster.png";
+monsterImage.src = "Resources/monster.png";
 export const poisonWeedImage = new Image();
-poisonWeedImage.src = "/Resources/poison_weed.png";
+poisonWeedImage.src = "Resources/poison_weed.png";
 export const poisonWeedSprayedImage = new Image();
-poisonWeedSprayedImage.src = "/Resources/poison_weed_sprayed.png";
+poisonWeedSprayedImage.src = "Resources/poison_weed_sprayed.png";
 export const eyeImage = new Image();
-eyeImage.src = "/Resources/monster_eye.png";
+eyeImage.src = "Resources/monster_eye.png";
 export const eyeShotImage = new Image();
-eyeShotImage.src = "/Resources/monster_eye_shot.png";
+eyeShotImage.src = "Resources/monster_eye_shot.png";
 
 
 export const fullGrownPic = new Image();
 export const sprayedPic = new Image();
 export const seedPic = new Image();
-fullGrownPic.src = "/Resources/good_grass_new.png";
-sprayedPic.src = "/Resources/good_grass_new_sprayed.png";
-seedPic.src = "/Resources/seeded.png"
+fullGrownPic.src = "Resources/good_grass_new.png";
+sprayedPic.src = "Resources/good_grass_new_sprayed.png";
+seedPic.src = "Resources/seeded.png"
 
 export const weedPic = new Image();
-weedPic.src = "/Resources/weed.png";
+weedPic.src = "Resources/weed.png";
 export const weedSprayedPic = new Image();
-weedSprayedPic.src = "/Resources/weedsprayed.png";
+weedSprayedPic.src = "Resources/weedsprayed.png";
 
 export const headImage = new Image();
-headImage.src = "/Resources/wurm_head.png";
+headImage.src = "Resources/wurm_head.png";
 export const bodyImage = new Image();
-bodyImage.src = "/Resources/wurm_body.png";
+bodyImage.src = "Resources/wurm_body.png";
 
 /*
   <data name="insect10" type="System.Resources.ResXFileRef, System.Windows.Forms">
